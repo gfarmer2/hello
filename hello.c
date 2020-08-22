cd50ac296a920a5bc2a68434b8ae21182687d89e
cberg7 <cberg7@huskers.unl.edu>
2020-08-22 14:07:34 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -11,7 +11,8 @@ int main(int argc, char **argv) {
     
     printf("Hello World, I'm Gavin Farmer!\n");
     printf("Computer Engineering\n");
-    
+    printf("Hello Gavin, I am Cory Berg.\n");
+    printf(I Am also trying to Major in Computer Engineering!!\n");
     return 0;
     
 }
